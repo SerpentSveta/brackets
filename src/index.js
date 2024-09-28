b91c35bbb8f2b6162d9d38a5e4fc98301b6bf113
SerpentSveta <sveta.aleksandrova@gmail.com>
2024-09-28 19:16:36 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  const openBrackets = [];
+  const bracketPairs = {};
+  const sameBrackets = [];
+
+  bracketsConfig.forEach(subArray => {
+    openBrackets.push(subArray[0]);
+    bracketPairs[subArray[1]] = subArray[0];
+    if (subArray[1] == subArray[0]) {
+      sameBrackets.push(subArray[0]);
+    }
+  })
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentElem = str[i];
+
+    if (sameBrackets.includes(currentElem)) {
+      if (stack[stack.length - 1] === currentElem) {
+        stack.pop();
+      } else {
+        stack.push(currentElem);
+      }
+      continue;
+    }
+
+    if (openBrackets.includes(currentElem)) {
+      stack.push(currentElem);
+    } else {
+      if (stack.length === 0 || stack[stack.length - 1] !== bracketPairs[currentElem]) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+
+  return stack.length === 0;
 }
